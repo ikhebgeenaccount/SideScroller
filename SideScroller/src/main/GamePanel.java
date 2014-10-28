adae58a744ca96bff835a2fb4904c7995603e470
ikhebgeenaccount <ikhebgeenaccount@users.noreply.github.com>
2014-10-28 13:29:21 +0100

    Changed MOVEPX
    More fluid motions.

--- SideScroller/src/main/GamePanel.java
+++ SideScroller/src/main/GamePanel.java
@@ -28,7 +28,7 @@ public class GamePanel extends JPanel implements KeyListener{
 		keys = new boolean[1000];
 		charx = 0;
 		chary = 0;
-		MOVEPX = 10;
+		MOVEPX = 5;
 		frame = 0;
 		gravity = new Gravity();
 		jump = new Jump();
