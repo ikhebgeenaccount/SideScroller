3e4fd5882042616adbeb830ed7b23eb5b46b190d
ikhebgeenaccount <lars.abbo@gmail.com>
2014-11-02 13:35:18 +0100

    Created constants for animationtypes.

--- SideScroller/src/main/champion/Champion.java
+++ SideScroller/src/main/champion/Champion.java
@@ -5,6 +5,7 @@ import java.awt.Image;
 public class Champion {
 	
 	private Image currentAnimationFrame;
+	private static final int WALK = 0, JUMP_START = 1, JUMP_TOP = 2, IDLE = 3, CAST_Q = 4, CAST_W = 5, CAST_E = 6, CAST_R = 7;
 	
 	public Champion(){
 			
