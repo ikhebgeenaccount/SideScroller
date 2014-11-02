fec3ef5cf3c145ac88766a78f88c0e7e8edfb3a9
ikhebgeenaccount <lars.abbo@gmail.com>
2014-11-02 17:17:32 +0100

    Added animation system.

--- SideScroller/src/main/champion/champions/AlphaGuy.java
+++ SideScroller/src/main/champion/champions/AlphaGuy.java
@@ -1,5 +1,11 @@
 package main.champion.champions;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 import main.Main;
@@ -9,13 +15,52 @@ import main.champion.Champion;
 public class AlphaGuy extends Champion{
 	
 	private Animation[] animations;
+	private Animation currentAnimation;
 	
 	public AlphaGuy(){
 		System.out.println("Loading animations...");
-		ClassLoader cldr = Main.class.getClassLoader();
-		animations = new Animation[8];
-		animations[Champion.WALK]= new Animation(); 
-		animations[Champion.WALK].addScene(new ImageIcon(cldr.getResource("char/img/alphaman/idle.png")).getImage(), 5);
+		ClassLoader cldr = this.getClass().getClassLoader();
+		animations = new Animation[8];		
+		try {
+			//Add the idle animation to animations[]
+			animations[Champion.IDLE]= new Animation(); 
+			BufferedImage idleSprite = ImageIO.read(cldr.getResourceAsStream("char/img/alphaguy/idle.png"));
+			animations[Champion.IDLE].addScene(idleSprite.getSubimage(0, 0, 50, 100), 1);
+			
+			//Add the walking animation to animations[]
+			animations[Champion.WALK]= new Animation();
+			BufferedImage walkSprite = ImageIO.read(cldr.getResourceAsStream("char/img/alphaguy/walk.png"));
+			animations[Champion.WALK].addScene(walkSprite.getSubimage(0, 0, 50, 100), 10);
+			animations[Champion.WALK].addScene(walkSprite.getSubimage(50, 0, 50, 100), 10);
+			animations[Champion.WALK].addScene(walkSprite.getSubimage(100, 0, 50, 100), 10);
+			currentAnimation = animations[Champion.IDLE];
+			currentAnimation.start(0);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			Main.quitGame();
+		}
+	}
+	
+	@Override
+	public void setAnimationType(int type, int frame){
+		//We only have to change the animationtype when it is now running another type, otherwise the animation will continuously
+		//reset itself
+		if(currentAnimation == animations[type]){
+			
+		}else{
+			currentAnimation = animations[type];
+			currentAnimation.start(frame);			
+		}
+	}
+	
+	@Override
+	public Image getCurrentAnimationImage(int frame){
+		return currentAnimation.getCurrentSceneImage();
+	}
+	
+	@Override
+	public void checkNextScene(int frame){
+		currentAnimation.nextScene(frame);
 	}
 
 }
