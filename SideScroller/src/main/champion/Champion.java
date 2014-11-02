7fd2b4ab51b3196d29c9e877c2b4dc244ddc2aa6
ikhebgeenaccount <lars.abbo@gmail.com>
2014-11-02 15:35:07 +0100

    Cleaned up Champion

--- SideScroller/src/main/champion/Champion.java
+++ SideScroller/src/main/champion/Champion.java
@@ -6,35 +6,10 @@ import main.animation.Animation;
 
 public class Champion {
 	
-	private Image currentAnimationFrame;
-	public static final int WALK = 0, JUMP_START = 1, JUMP_TOP = 2, IDLE = 3, CAST_Q = 4, CAST_W = 5, CAST_E = 6, CAST_R = 7;
+	public static final int IDLE = 0, WALK = 1, JUMP = 2, FALL = 3, CAST_Q = 4, CAST_W = 5, CAST_E = 6, CAST_R = 7;
 		
 	public Champion(){
 			
-	}
-	
-	private class Q{
-		
-	}
-	
-	private class W{
-		
-	}
-	
-	private class E{
-		
-	}
-	
-	private class R{
-		
-	}
-	
-	public Image getAnimationFrame(){
-		return currentAnimationFrame;
-	}
-	
-	public void setAnimationFrame(Image animationFrame){
-		currentAnimationFrame = animationFrame;
 	}
 
 }
