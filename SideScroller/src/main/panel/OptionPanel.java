ddfe4a53a5bf8b3868eaf7433ab4e957b96ebefb
ikhebgeenaccount <lars.abbo@gmail.com>
2014-11-06 19:18:48 +0100

    Started with OptionPanel.

--- SideScroller/src/main/panel/OptionPanel.java
+++ SideScroller/src/main/panel/OptionPanel.java
@@ -1,5 +1,22 @@
 package main.panel;
 
-public class OptionPanel {
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JPanel;
+
+public class OptionPanel extends JPanel{
+	
+	private GridBagConstraints c;
+	
+	public OptionPanel(){
+		setLayout(new GridBagLayout());
+		
+		//Set layout properties
+		c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.WEST;
+		
+		
+	}
 
 }
