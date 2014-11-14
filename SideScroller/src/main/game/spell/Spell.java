a68e28aa32974fc353107dd1c1843deda9786818
ikhebgeenaccount <ikhebgeenaccount@users.noreply.github.com>
2014-11-14 12:01:17 +0100

    Created basics for Spell.

--- SideScroller/src/main/game/spell/Spell.java
+++ SideScroller/src/main/game/spell/Spell.java
@@ -1,5 +1,30 @@
 package main.game.spell;
 
 public class Spell {
+  
+  public int[] coordinates;
+  public int range;
+  public int damage;
+  
+  public Animation[] animations;
+  public static final int APPEAR = 0, TRAVEL = 1, HIT = 2;
+  
+  private boolean isFired;
+  
+  public Spell(){
+    
+  }
+  
+  public void fire(){
+    
+  }
+  
+  public boolean isFired(){
+    return isFired;
+  }
+  
+  public int[] getCoordinates(){
+    return coordinates;
+  }
 
 }
