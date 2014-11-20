51b01de87bc52cd3b1e4b05c8530ba4322c69019
ikhebgeenaccount <lars.abbo@gmail.com>
2014-11-20 19:02:10 +0100

    Fixed tabs, added some methods.

--- SideScroller/src/main/game/spell/Spell.java
+++ SideScroller/src/main/game/spell/Spell.java
@@ -1,5 +1,8 @@
 package main.game.spell;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 import main.game.animation.Animation;
 import main.game.coordinate.Coordinate;
 
@@ -21,16 +24,16 @@ public class Spell {
   }
   
   public Spell(int range, int damage){
-  	this.range = range;
-  	this.damage = damage;
+	  this.range = range;
+	  this.damage = damage;
   }
   
   public void setDamage(int damage){
-  	this.damage = damage;
+	  this.damage = damage;
   }
   
   public void setRange(int range){
-  	this.range = range;
+	  this.range = range;
   }
   
   public void fire(){
@@ -38,23 +41,38 @@ public class Spell {
   }
   
   public boolean isFired(){
-    return isFired;
+	  return isFired;
   }
   
   public Coordinate getCoordinates(){
-    return coordinates;
+	  return coordinates;
   }
   
   public void setAnimationType(int type) {
-		//We only have to change the animationtype when it is now running another type, otherwise the animation will continuously
-		//reset itself
-		if(currentAnimationType == type){
+	  //We only have to change the animationtype when it is now running another type, otherwise the animation will continuously
+	  //reset itself
+	  if(currentAnimationType == type){
 			
-		}else{
-			currentAnimationType = type;
-			currentAnimation = animations[type];
-			currentAnimation.start();			
-		}
-	}
+	  }else{
+		  currentAnimationType = type;
+		  currentAnimation = animations[type];
+	  }
+  }
+  
+  public Image getCurrentAnimationImage() {
+	  return currentAnimation.getCurrentSceneImage();
+  }
+	
+  public void checkNextScene(){
+	  currentAnimation.nextScene();
+  }
+	
+  public void addAnimation(int type, BufferedImage animationSprite, int sceneLength){
+	  animations[type] = new Animation();
+	  int scenes = animationSprite.getWidth() / 50;
+	  for(int i = 0; i < scenes; i++){
+		  animations[type].addScene(animationSprite.getSubimage(50 * i, 0, 50, 100), sceneLength);
+	  }
+  }
 
 }
