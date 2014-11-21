becdc45443ed399d0d91f9a07a5b398307880c03
ikhebgeenaccount <lars.abbo@gmail.com>
2014-11-21 17:48:25 +0100

    Started with Spell.

--- SideScroller/src/main/game/object/spell/Spell.java
+++ SideScroller/src/main/game/object/spell/Spell.java
@@ -1,28 +1,28 @@
 package main.game.object.spell;
 
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-
-import main.game.animation.Animation;
 import main.game.coordinate.Coordinate;
 import main.game.object.GameObject;
 
 public class Spell extends GameObject{
 
+	public static final int APPEAR = 0, TRAVEL = 1, HIT = 2;
+	
 	private int range;
 	private int damage;
-
-	public static final int APPEAR = 0, TRAVEL = 1, HIT = 2;
+	
+	private Coordinate coordinates;
+	private Coordinate startCoordinates;
 
 	private boolean isFired;
 
 	public Spell(){
-
+		
 	}
 
-	public Spell(int range, int damage){
-		this.range = range;
-		this.damage = damage;
+	public Spell(int range, int damage, int speed){
+		setRange(range);
+		setDamage(damage);
+		setSpeed(speed);
 	}
 
 	public void setDamage(int damage){
@@ -33,8 +33,27 @@ public class Spell extends GameObject{
 		this.range = range;
 	}
 
-	public void fire(){
-		
+	public void fire(Coordinate startCoodinates){
+		this.startCoordinates = startCoordinates;
+		coordinates = this.startCoordinates;
+	}
+	
+	public void move(boolean moveLeft){ //moveLeft = true, if moving left, false when moving right
+		if(moveLeft){
+			//Spell is moving to the left
+			if(coordinates.x - startCoordinates.x > 0){
+				//Move
+			}else{
+				//Out of range
+			}
+		}else{
+			//Spell is moving to the right
+			if(startCoordinates.x - coordinates.x > 0){
+				//Move
+			}else{
+				//Out of range
+			}
+		}
 	}
 
 	public boolean isFired(){
