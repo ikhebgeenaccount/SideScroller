d3a8ec83c0ba94ed413cc728cd59cb86365f5203
ikhebgeenaccount <ikhebgeenaccount@users.noreply.github.com>
2014-11-25 10:48:37 +0100

    Added paintComponent()

--- SideScroller/src/main/gui/panel/StatusBar.java
+++ SideScroller/src/main/gui/panel/StatusBar.java
@@ -5,8 +5,20 @@ public class StatusBar extends JPanel{
 	private Image qIcon, wIcon, eIcon, rIcon;
 	
 	public StatusBar(){
-		setPreferredSize(new Dimension(1000, 50));
+		setPreferredSize(new Dimension(1000, 80));
 		
 		setLayout(new FlowLayout(FlowLayout.CENTER));
+	}
+	
+	@Override
+	public void paintComponent(Graphics g){
+		//Draw spell icons
+		g.drawImage(qIcon, 385, 0, null);
+		g.drawImage(wIcon, 445, 0, null);
+		g.drawImage(eIcon, 505, 0, null);
+		g.drawImage(rIcon, 565, 0, null);
+		
+		//Draw remaining cooldowns
+		
 	}
 }
