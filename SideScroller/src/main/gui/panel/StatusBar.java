f95af0fabdc3a1f8f026f40fa16f12a600cd9c30
ikhebgeenaccount <ikhebgeenaccount@users.noreply.github.com>
2014-11-25 11:27:25 +0100

    Added grayed out icons.

--- SideScroller/src/main/gui/panel/StatusBar.java
+++ SideScroller/src/main/gui/panel/StatusBar.java
@@ -3,6 +3,7 @@ package main.gui.panel;
 public class StatusBar extends JPanel{
 	
 	private Image qIcon, wIcon, eIcon, rIcon;
+	private Image qIconGrayed, wIconGrayed, eIconGrayed, rIconGrayed;
 	
 	public StatusBar(){
 		setPreferredSize(new Dimension(1000, 80));
@@ -10,24 +11,50 @@ public class StatusBar extends JPanel{
 		//Load pics
 		try{
 			qIcon = ImageIO.read(getClass().getClassLoader().getResourceAsStream("img/char" + Main.getGamePanel().getCharacterName() + "qIcon.png"));
+			qIconGrayed = ImageIO.read(getClass().getClassLoader().getResourceAsStream("img/char" + Main.getGamePanel().getCharacterName() + "qIconGrayed.png"));
 			wIcon = ImageIO.read(getClass().getClassLoader().getResourceAsStream("img/char" + Main.getGamePanel().getCharacterName() + "wIcon.png"));
+			wIconGrayed = ImageIO.read(getClass().getClassLoader().getResourceAsStream("img/char" + Main.getGamePanel().getCharacterName() + "wIconGrayed.png"));
 			eIcon = ImageIO.read(getClass().getClassLoader().getResourceAsStream("img/char" + Main.getGamePanel().getCharacterName() + "eIcon.png"));
+			eIconGrayed = ImageIO.read(getClass().getClassLoader().getResourceAsStream("img/char" + Main.getGamePanel().getCharacterName() + "eIconGrayed.png"));
 			rIcon = ImageIO.read(getClass().getClassLoader().getResourceAsStream("img/char" + Main.getGamePanel().getCharacterName() + "rIcon.png"));
+			rIconGrayed = ImageIO.read(getClass().getClassLoader().getResourceAsStream("img/char" + Main.getGamePanel().getCharacterName() + "rIconGrayed.png"));
 		}
 	}
 	
 	@Override
 	public void paintComponent(Graphics g){
+		//Get cooldowns
+		int qcd = Main.getGamePanel().getCharacter.getRemainingCooldown(Champion.Q);
+		int wcd = Main.getGamePanel().getCharacter.getRemainingCooldown(Champion.W);
+		int ecd = Main.getGamePanel().getCharacter.getRemainingCooldown(Champion.E);
+		int rcd = Main.getGamePanel().getCharacter.getRemainingCooldown(Champion.R);
+		
 		//Draw spell icons
-		g.drawImage(qIcon, 385, 0, null);
-		g.drawImage(wIcon, 445, 0, null);
-		g.drawImage(eIcon, 505, 0, null);
-		g.drawImage(rIcon, 565, 0, null);
+		if(qcd == 0){
+			g.drawImage(qIcon, 385, 0, null);
+		}else{
+			g.drawImage(qIconGrayed, 385, 0, null);
+		}
+		if(wcd == 0){
+			g.drawImage(wIcon, 385, 0, null);
+		}else{
+			g.drawImage(wIconGrayed, 385, 0, null);
+		}
+		if(ecd == 0){
+			g.drawImage(eIcon, 385, 0, null);
+		}else{
+			g.drawImage(eIconGrayed, 385, 0, null);
+		}
+		if(rcd == 0){
+			g.drawImage(rIcon, 385, 0, null);
+		}else{
+			g.drawImage(rIconGrayed, 385, 0, null);
+		}
 		
 		//Draw remaining cooldowns
-		g.drawString(Main.getGamePanel().getCharacter().getRemainingCooldown(Champion.Q), 385, 52, null);
-		g.drawString(Main.getGamePanel().getCharacter().getRemainingCooldown(Champion.W), 445, 52, null);
-		g.drawString(Main.getGamePanel().getCharacter().getRemainingCooldown(Champion.E), 505, 52, null);
-		g.drawString(Main.getGamePanel().getCharacter().getRemainingCooldown(Champion.R), 565, 52, null);
+		g.drawString(String.valueOf(qcd), 385, 52, null);
+		g.drawString(String.valueOf(wcd), 445, 52, null);
+		g.drawString(String.valueOf(ecd), 505, 52, null);
+		g.drawString(String.valueOf(rcd), 565, 52, null);
 	}
 }
