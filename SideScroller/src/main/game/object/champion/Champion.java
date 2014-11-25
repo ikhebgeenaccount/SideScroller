197a15dd13b800d738793255257e30d6f25db70a
ikhebgeenaccount <ikhebgeenaccount@users.noreply.github.com>
2014-11-25 11:02:03 +0100

    Added getRemainingCooldown().

--- SideScroller/src/main/game/object/champion/Champion.java
+++ SideScroller/src/main/game/object/champion/Champion.java
@@ -7,6 +7,7 @@ import main.game.object.spell.Spell;
 public class Champion extends GameObject{
 	
 	public static final int IDLE = 0, WALK_LEFT = 1, WALK_RIGHT = 2, JUMP = 3, FALL = 4, CAST_Q = 5, CAST_W = 6, CAST_E = 7, CAST_R = 8;
+	public static final int Q = 0, W = 1, E = 2, R = 3;
 	
 	private Spell q, w, e, r;
 	
@@ -31,6 +32,20 @@ public class Champion extends GameObject{
 	
 	public void castR(Coordinate startCoordinate){
 		r.fire(startCoordinate);
+	}
+	
+	public String getRemainingCooldown(int spell){
+		if(spell == Q){
+			return q.getRemainingCooldown();
+		}else if(spell == W){
+			return w.getRemainingCooldown();
+		}else if(spell == E){
+			return e.getRemainingCooldown();
+		}else if(spell == r){
+			return r.getRemainingCooldown();
+		}else{
+			return "Spell not found";
+		}
 	}
 
 }
