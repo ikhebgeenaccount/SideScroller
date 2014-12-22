a721bc7bd2037c5fd39fa378dc81b7153a3eb680
Lars <lars.abbo@gmail.com>
2014-12-22 13:58:26 +0100

    Added setCoordinates(Coordinate).

--- SideScroller/src/main/game/coordinate/Coordinate.java
+++ SideScroller/src/main/game/coordinate/Coordinate.java
@@ -13,9 +13,14 @@ public class Coordinate{
         this.y = y;
     }
     
-    public void setCoordinate(int x, int y){
+    public void setCoordinates(int x, int y){
         this.x = x;
         this.y = y;
+    }
+    
+    public void setCoordinates(Coordinate coordinates){
+    	this.x = coordinates.x;
+    	this.y = coordinates.y;
     }
     
     public void setX(int x){
