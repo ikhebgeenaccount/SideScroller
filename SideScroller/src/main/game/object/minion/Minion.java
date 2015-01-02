7886d448d07e55c46e09818e40fed12510771013
Lars <lars.abbo@gmail.com>
2015-01-02 14:53:43 +0100

    Created Minion.

---
+++ SideScroller/src/main/game/object/minion/Minion.java
@@ -0,0 +1,20 @@
+package main.game.object.minion;
+
+import main.game.object.GameObject;
+
+public class Minion extends GameObject {
+	
+	private static final int WALK_RIGHT = 0, WALK_LEFT = 1, ATTACK = 2, DIE = 3; 
+	
+	private int range;
+	
+	public Minion(int speed, int range){
+		setSpeed(speed);
+		setRange(range);
+	}
+	
+	public void setRange(int range){
+		this.range = range;
+	}
+
+}
