a72bab4bd6beada6fe1bf53558ac9b99819fbdd7
ikhebgeenaccount <lars.abbo@gmail.com>
2015-09-20 18:54:54 +0200

    Added start of MessageBox.

---
+++ SideScroller/src/main/gui/MessageBox.java
@@ -0,0 +1,27 @@
+package main.gui;
+
+import javax.swing.JPanel;
+
+public class MessageBox extends JPanel{
+	
+	/*
+	 * 			.....................................
+	 * 			.									.
+	 * 			.			  <Message>				.
+	 * 			.									.
+	 * 			.									.
+	 * 			.									.
+	 * 			.									.
+	 * 			.			   ......				.
+	 * 			.			   . OK .				.
+	 * 			.			   ......				.
+	 * 			.....................................
+	 */
+	
+	
+	
+	public MessageBox(String message){
+		
+	}
+
+}
