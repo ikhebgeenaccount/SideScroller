8f68dc6fc24c488cf3e3183c487fb1b6921713de
ikhebgeenaccount <lars.abbo@gmail.com>
2015-09-21 19:52:01 +0200

    Size and UI are set, only needs method.

--- SideScroller/src/main/gui/MessageBox.java
+++ SideScroller/src/main/gui/MessageBox.java
@@ -1,5 +1,13 @@
 package main.gui;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class MessageBox extends JPanel{
@@ -19,8 +27,28 @@ public class MessageBox extends JPanel{
 	 */
 	
 	
-	
-	public MessageBox(String message){
+	public MessageBox(String message/*, void methodToExecute*/){
+		
+		this.setLayout(new GridBagLayout());
+		this.setPreferredSize(new Dimension(200, 150));
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.CENTER;
+		c.gridy = 0;
+		c.gridx = 0;
+		
+		this.add(new JLabel(message), c);
+		
+		c.gridy++;
+		
+		JButton okButton = new JButton("Ok");
+		okButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//Execute method
+			}
+		});
+		
+		this.add(okButton, c);
 		
 	}
 
