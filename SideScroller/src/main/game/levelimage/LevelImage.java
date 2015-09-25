edaea7120f9ff1117c0ed5606011beb55887ec1b
ikhebgeenaccount <lars.abbo@gmail.com>
2015-09-25 19:34:40 +0200

    Added LevelImage.

---
+++ SideScroller/src/main/game/levelimage/LevelImage.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package main.game.levelimage;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+import main.Main;
+
+/**
+ * @author ikhebgeenaccount
+ * @version 24 sep. 2015
+ */
+public class LevelImage extends BufferedImage {
+	
+	private Graphics2D g;
+	
+	//Images
+	private Image ground, air, grass_ground;
+	
+	private String theme;
+	
+	public LevelImage(int[][] level, String theme){
+		super(level[0].length * 50, level.length * 50, BufferedImage.TYPE_INT_RGB);
+
+		ClassLoader cldr = this.getClass().getClassLoader();
+		air = new ImageIcon(cldr.getResource("img/landscape/" + theme + "/air.png")).getImage();
+		ground = new ImageIcon(cldr.getResource("img/landscape/" + theme + "/ground.png")).getImage();
+		grass_ground = new ImageIcon(cldr.getResource("img/landscape/" + theme + "/grass-ground.png")).getImage();
+		
+		this.theme = theme;
+		
+		g = createGraphics();
+		
+		for(int x = 0; x < level[0].length; x++){
+			for(int y = 0; y < level.length; y++){
+				switch(level[y][x]){
+				case 0:g.drawImage(air, 50 * x, 50 * y, null);
+					break;
+				case 1:g.drawImage(ground, 50 * x, 50 * y, null);
+					break;	
+				case 2:g.drawImage(grass_ground, 50 * x, 50 * y, null);
+					break;
+				default:g.drawImage(air, 50 * x, 50 * y, null);
+					break;
+				}
+			}
+		}
+	}
+
+}
---
+++ SideScroller/src/main/game/levelimage/LevelImageTest.java
@@ -0,0 +1,35 @@
+package main.game.levelimage;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class LevelImageTest extends JFrame {
+	
+	public LevelImageTest(LevelImage LevelImage){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(new ContentPanel(LevelImage));
+		pack();
+		setVisible(true);
+	}
+	
+	private static class ContentPanel extends JPanel{
+		
+		private LevelImage LevelImage;
+		
+		ContentPanel(LevelImage LevelImage){
+			 setPreferredSize(new Dimension(LevelImage.getWidth(), LevelImage.getHeight()));
+			 this.LevelImage = LevelImage;
+		}
+		
+		@Override
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			g.drawImage(LevelImage, 0, 0, null);
+			g.dispose();
+		}
+	}
+
+}
