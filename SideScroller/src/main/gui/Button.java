4f78705a6835900855e4c62a66305cb66797faad
ikhebgeenaccount <lars.abbo@gmail.com>
2015-09-29 00:10:37 +0200

    Changed Button Color and removed focus from Button.

--- SideScroller/src/main/gui/Button.java
+++ SideScroller/src/main/gui/Button.java
@@ -1,5 +1,7 @@
 package main.gui;
 
+import java.awt.Color;
+
 import javax.swing.JButton;
 
 import main.Main;
@@ -16,6 +18,9 @@ public class Button extends JButton {
 	public Button(String text){
 		setText(text);
 		setFont(Main.getFont(20));
+		setBackground(new Color(59, 89, 182));
+        setForeground(Color.WHITE);
+        setFocusPainted(false);
 	}
 
 }
